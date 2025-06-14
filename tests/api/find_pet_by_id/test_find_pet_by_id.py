11bc38c3b5ec2a88a74c2e6bb68c0c0244cc993a
Alvin Heng <alvin.heng@shopee.com>
2025-06-14 15:48:21 +0700

    add getignore and fix get_pet_by_id case

--- tests/api/find_pet_by_id/test_find_pet_by_id.py
+++ tests/api/find_pet_by_id/test_find_pet_by_id.py
@@ -1,12 +1,16 @@
 import pytest
-from utils.requests import get_pet
+from utils.requests import get_pet, find_pet_by_status
 from utils.status_codes import StatusCode
 from schemas.pet import PetResponse
-from utils.tools import log_response
 
-@pytest.fixture(params=[9223372036854740750])
-def id(request):
-    return request.param
+
+@pytest.fixture
+def id():
+    # Dynamically fetch an available pet ID
+    res = find_pet_by_status("available")
+    assert res.status_code == StatusCode.SUCCESS
+    pet_id = res.json()[0]["id"]
+    return pet_id
 
 def test_can_get_pet_by_id(id):
     # find pet by id
